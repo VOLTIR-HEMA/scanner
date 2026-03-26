5d0aa2f7821053a33de5738945bd5bc6a0b44a11
VOLTIR-HEMA <96622772+VOLTIR-HEMA@users.noreply.github.com>
2026-03-26 12:12:01 +0200

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // ضع رابط المونجو الخاص بك هنا وتأكد من كتابة الباسوورد
-const mongoURI = "رابط_مونجو_الخاص_بك_هنا";
+const mongoURI = "mongodb+srv://justscan_db_user:Hema@2003@justscan.9w2pw5s.mongodb.net/?appName=JustScan";
 
 exports.handler = async (event, context) => {
     // الاتصال بقاعدة البيانات
